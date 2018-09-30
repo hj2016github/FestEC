19db7fd0ef502605c7eb7cb16c2a890543b7edae
hj2016github <linzhiqiu@gmail.com>
2018-09-30 10:38:28 +0800

    配置,buidler模式;

---
+++ General_core/src/main/java/com/gehj/general_core/ConfigKeys.java
@@ -0,0 +1,19 @@
+package com.gehj.general_core;
+
+/**
+ * Created by 傅令杰 on 2017/3/29
+ */
+
+public enum ConfigKeys {//ConfigType 枚举类;
+    API_HOST,//网络请求域名;
+    APPLICATION_CONTEXT,
+    CONFIG_READY,//初始化是否配置完成;
+    ICON,
+    LOADER_DELAYED,
+    INTERCEPTOR,
+    WE_CHAT_APP_ID,
+    WE_CHAT_APP_SECRET,
+    ACTIVITY,
+    HANDLER,
+    JAVASCRIPT_INTERFACE
+}
